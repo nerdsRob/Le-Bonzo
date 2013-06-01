311ba7d4d615bf92ca68d5dbf145eff743bb22a6
nerdsRob <robert@soundcloud.com>
2013-06-01 16:13:51 +0200

    Synth notes C4 - A4

--- js/drumkit.js
+++ js/drumkit.js
@@ -1,25 +1,28 @@
-// Samples
+// Samples freesound.org
 var kick  	= "samples/kick.mp3";
 var snare 	= "samples/snare.mp3";
-var ride 	= "samples/ride.mp3";
+var ride 	= "samples/hihat.wav";
 var splash 	= "samples/splash.mp3";
 
 // Web Audio API
 window.AudioContext = window.AudioContext || window.webkitAudioContext;
 var context = new AudioContext();
 var source = null;
+var oscillator = context.createOscillator();
+var amp = context.createGain();
 
 // Add Keyboard listener
 window.onload = function() {
 	document.addEventListener("keydown", checkKey, false);
+	document.addEventListener("keyup", stopNote, false);
 }
 
 // Playback
 function playSound(buffer) {
-  source = context.createBufferSource(); 	 
+  source = context.createBufferSource(); 
   source.buffer = buffer;                   
-  source.connect(context.destination);       
-  source.start(0);                           
+  source.connect(context.destination);      
+  source.start(0);                          
 }
 
 function fetchSound(sound) {
@@ -41,6 +44,29 @@ function fetchSound(sound) {
     request.send();
 }
 
+function playNote(frequency) {
+	oscillator.frequency.value = frequency;
+    amp.gain.value = 0;
+    
+    oscillator.connect(amp);
+    amp.connect(context.destination);
+    oscillator.start(0);
+    
+    var now = context.currentTime;
+    oscillator.frequency.setValueAtTime(frequency, now);
+    
+    amp.gain.cancelScheduledValues(now);
+    amp.gain.setValueAtTime(amp.gain.value, now);
+    amp.gain.linearRampToValueAtTime(0.8, context.currentTime + 0.1);
+}
+
+function stopNote() {
+    var now = context.currentTime;
+	amp.gain.cancelScheduledValues(now);
+    amp.gain.setValueAtTime(amp.gain.value, now);
+    amp.gain.linearRampToValueAtTime(0.0, context.currentTime + 1.0);
+}
+
 function checkKey(event) {
     event = event || window.event;
 	
@@ -53,6 +79,17 @@ function checkKey(event) {
 		case 39: fetchSound(3); break;
 		// Down arrow
 		case 40: fetchSound(4); break;
+		// W
+		case 87: playNote(262); break; // C4
+		// A
+		case 65: playNote(294); break; // D4
+		// S
+		case 83: playNote(330); break; // E4
+		// D
+		case 68: playNote(349); break; // F4
+		// F
+		case 70: playNote(392); break; // G4
+		// G
+		case 71: playNote(440); break; // A4
 	}
-	updateText();
 }
